249475dc012119b283d037f213edc728e035ebbd
zhang33feng <zhangfeng_xfdsn@163.com>
2018-08-30 13:36:34 +0800

    添加了登录的sql语句

---
+++ snxy-user-agent-domain/src/main/java/com/snxy/user/agent/domain/UserIdentity.java
@@ -0,0 +1,18 @@
+package com.snxy.user.agent.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+//查询用户身份的映射表
+public class UserIdentity {
+    private Long systemUserId;
+    private Integer identyId;
+    private Byte isActive;
+    private String identyName;
+}
